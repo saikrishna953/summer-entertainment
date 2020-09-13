11f208b3a2c4feffc8f35c4228c4bdf491b8cc51
Sai C <roddick.sai@gmail.com>
2020-09-13 17:11:57 +0530

    Fix for not showing user consent

--- src/main/java/org/acme/Sheets/Credential.java
+++ src/main/java/org/acme/Sheets/Credential.java
@@ -4,14 +4,14 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
 import org.acme.Constants;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Collections;
 
 public class Credential {
 
@@ -31,11 +31,12 @@ public class Credential {
 
     public static com.google.api.client.auth.oauth2.Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        System.out.println("In getCredentials() method");
+        /*System.out.println("In getCredentials() method");
         InputStream in = SheetOperations.class.getResourceAsStream(Constants.CREDENTIALS_FILE_PATH);
         if (in == null) {
             throw new FileNotFoundException("Resource not found: " + Constants.CREDENTIALS_FILE_PATH);
         }
+
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(Constants.JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
@@ -45,6 +46,9 @@ public class Credential {
                 .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");*/
+        GoogleCredential credential = GoogleCredential.fromStream(SheetOperations.class.getResourceAsStream(Constants.CREDENTIALS_FILE_PATH))
+                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+        return credential;
     }
 }
